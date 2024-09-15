d39286d2ad65197ca15e5e0565d2faeaacfb1b26
AthithianV <athithianpkt2001@gmail.com>
2024-09-15 11:13:14 +0530

    update on index

--- athithian_v/coding_task/src/index.ts
+++ athithian_v/coding_task/src/index.ts
@@ -1,19 +1,23 @@
 import app from "./app";
 import dotenv from "dotenv";
 import { createLogDirectory } from "./util/createlogs";
+import { Server } from "socket.io";
+import { createServer } from "http";
 
 dotenv.config();
 
 
 const port = process.env.PORT || 3000;
 
+const server = createServer(app);
+const io = new Server(server);
 
-async function main(){
-    app.listen(port, async ()=>{
-        console.log(`Server is listening at Port: ${port}`);
-        createLogDirectory();
-    });
-}
+io.on("connection", (socket)=>{
+    console.log(socket.id);
+});
 
-main();
+server.listen(port, async ()=>{
+    console.log(`Server is listening at Port: ${port}`);
+    createLogDirectory();
+});
 
